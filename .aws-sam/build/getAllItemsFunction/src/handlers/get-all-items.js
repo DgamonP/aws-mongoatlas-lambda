4dae7f4901312fe07623a31122089d4291cf7464
dell <d.gamon.p@gmail.com>
2022-04-23 00:18:06 -0400

    funcional estado

--- .aws-sam/build/getAllItemsFunction/src/handlers/get-all-items.js
+++ .aws-sam/build/getAllItemsFunction/src/handlers/get-all-items.js
@@ -3,49 +3,42 @@ const tableName = process.env.SAMPLE_TABLE;
 
 // Create a DocumentClient that represents the query to add an item
 const mongo = require('mongodb');
-const { MongoClient } = mongo;
-let db = null;
+const { MongoClient, ServerApiVersion } = mongo;
+//let db = null;
 
 let connectToDatabase = (uri, dbName) => {
-  if (db && db.serverConfig.isConnected()) {
-    return Promise.resolve(db);
-  }
-  return MongoClient.connect(uri, { poolSize: 10, useNewUrlParser: true, useUnifiedTopology: true }).then(client => {
-    db = client.db(dbName);
+  // if (db && db.serverConfig.isConnected()) {
+  //   return Promise.resolve(db);
+  // }
+  return MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(client => {
+    const db = client.db(dbName);
     return db;
   });
 };
-
-let getTodoById = (db, table, query) => {
+let getTodos = (db, table, query) => {
   return db
     .collection(table)
-    .findOne(query);
+    // .findOne(query);
+    .find({}).toArray();
 }
-
 exports.getAllItemsHandler = async (event) => {
     if (event.httpMethod !== 'GET') {
         throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
     }
     // All log statements are written to CloudWatch
-    console.info('received:', event);
+    // console.info('received:', event);
 
-    const mongoUri = process.env.MONGODB_URI 
-    const mongoDBName = process.env.MONGODB_NAME
-
+    // const mongoUri = process.env.MONGODB_URI 
+    // const mongoDBName = process.env.MONGODB_NAME
+    const mongoUri = 'mongodb+srv://dbDevelop:adminalfa@cluster-develop.w45zq.mongodb.net/deltaDev'
+    const mongoDBName = 'deltaDev'
+    console.log("conections",mongoUri,mongoDBName)
     const dbConnection = await connectToDatabase(mongoUri, mongoDBName);
-    const todo = await getTodoById(dbConnection, "todos", { id: 100 });
-
+    const todo = await getTodos(dbConnection, "users", {  });
+    console.log(todo)
     return {
         statusCode: 200,
         body: JSON.stringify(todo),
     };
 
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(items)
-    };
-
-    // All log statements are written to CloudWatch
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
 }
